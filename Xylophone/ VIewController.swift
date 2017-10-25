ed196dca06ff865845fc45701dca673919d06222
Marcin Kobiela <kobsonauta@89-64-144-191.dynamic.chello.pl>
2017-10-25 23:31:30 +0200

    Initial commit: created project and added all the resources.

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Kobsonauta on 25.10.2017.
+//  Copyright © 2017 Kobsonauta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+  
+
+}
+
