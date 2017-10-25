3a504e5841fec1a8de218e00d2d90f934a22e95d
Marcin Kobiela <kobsonauta@89-64-144-191.dynamic.chello.pl>
2017-10-26 00:48:31 +0200

    Playing sound added.

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
-    
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +20,14 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
     }
     
   
